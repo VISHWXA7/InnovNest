27bf8e1bd31639154ea11723f5fa625b55df3e03
Vishwaa <vishwaa.3511@gmail.com>
2024-10-08 22:03:46 +0530

    update commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+export const metadata ={
+  title: 'InnovNest',
+  description: 'A Next.js 14 App'
+};
+
+const inter = Inter({ subsets: ['latin']});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <ClerkProvider>
+        <html lang="en">
+          <body className={inter.className}>
+            <Topbar />
+
+            <main>
+                <LeftSidebar />
+                    
+                    <section className="main-container">
+                      <div className="w-full max-w-4xl">
+                        {children}
+                      </div>
+                    </section>
+
+                <RightSidebar />
+            </main>
+
+            <Bottombar />
+          </body>
+        </html>
+      </ClerkProvider>
+  );
+}
